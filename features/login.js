197f4bad0a55639c056dfb6a924a65efe05fe958
WolliRus <krytovsky@yandex.ru>
2017-07-28 12:58:48 +0300

    One source for URL

--- features/login.js
+++ features/login.js
@@ -1,27 +1,10 @@
 exports.command = function (loginData,passwordData) {
 
+  var fs = require('fs');
   var site = require("../CONST/constants.js");
-  var fs = require('fs'); // подключаем модуль для работы с файловой системой
-  var login = require("../TestData/LoginData.js"); // подключаем базу
-  var currentSite = site.TOMASKZTEST;
-  var currentDomain = "t3";
-  var prot = site.HTTPS;
-  var url = prot + currentSite + currentDomain;
-
-  switch (currentSite) {
-    case "satom.": ref = "/?ref=d4079";
-    break;
-    case "zakupka.": ref = "/?ref=d3179";
-      break;
-    case "tomas.by": ref = "/?ref=d4079";
-      break;
-    case "tomas.kz": ref = "/?ref=d4195";
-      break;
-    default: ref = '';
-
-    var refUrl = prot + currentSite + currentDomain + ref;
-
-  }
+  var url = site.URL;
+  var refUrl = site.URL + site.REF;
+  var randomNumber = Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
 
     this.url(url + '/registration/login/')
     this.maximizeWindow()
