0d0347eb7adeab1a10e80b80ce4b55100a335343
WolliRus <krytovsky@yandex.ru>
2017-08-01 17:01:30 +0300

    Кнопки купить, написать нам

--- tests/pattern.js
+++ tests/pattern.js
@@ -1,53 +1,58 @@
+var login = require("../TestData/LoginData.js"); // подключаем базу
 var fs = require('fs');
 var site = require("../CONST/constants.js");
 var url = site.URL;
 var refUrl = site.URL + site.REF;
-var login = require("../TestData/LoginData.js"); // подключаем базу
-var randomNumber = Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
+var randomNumber = Math.floor(Math.random() * (100000 - 1 + 1)) + 1;;
 
-
-var clientLogin = 'apuddosso-5781@yopmail.com';
-var clientPassword = 'test';
-var secondCompanyLogin = 'sunkar@test.com';
-var secondCompanyPassword = 'test';
-var targetUrl = '/p/187184-utyuzhishche/';
-
-// для этого теста переопределяем юрл на томас.кз
-
-
-// начинаем тесты
 module.exports = {
 
 tags: ['testrun'],
-
-'Написать компании c логином, кирилица' : function (browser) {
+/*
+'Проверка, что кнопка купить у товара в наличии' : function (browser) {
   browser
-  .login(clientLogin, clientPassword)
-  .url(url + targetUrl)
-
-  .getText('.product__name', function (result) {
-  product_name = result.value
-  console.log(product_name);
+  .url(url + "/p/174638-17-opg-17-gidroksiprogesteron/")
+  .maximizeWindow()
+  .assert.containsText('.p_info td span[class="b-btn__name gradient"]', "Купить", "Кнопка действительно называется купить")
+  .click('.p_info td span[class="b-btn__name gradient"]', function () {
+    this.waitForElementVisible('#postavshik>div', 5000, 'Увидели кнопку отправки в корзине, довольны')
   })
+  .end()
+},
 
-  .waitForElementPresent('.cabinet-footer-copyrights', 5000, function () {
-  this.click('.contact_firm.dashed.on_hover__link.event_ga')
+'Проверка, что кнопка купить пропала у товара не в наличии' : function (browser) {
+  browser
+  .url(url + "/p/174577-anti-hav-markery-virusnogo-gepatita-a/")
+  .maximizeWindow()
+  .assert.containsText('.p-presence-2', "Нет в наличии", "Кнопки купить нет, уведомление 'нет в наличии'")
+  .click('#add_to_waitlist', function () {
+    this.waitForElementVisible('#waitlist_dialog', 5000, 'Увидели форму заявки в список ожиданий')
   })
-  .waitForElementVisible('#contact-now-dialog', 5000, function () {
-    this.setValue('#message', 'Сообщение с латиницей')
-    this.click('#send_feedback_form')
+  .waitForElementVisible('#postavshik>div', 5000, 'Увидели кнопку отправки в корзине, довольны')
+  .waitForElementNotPresent('.p_info td span[class="b-btn__name gradient', 5000, "Кнопки купить в коде нет")
+  .end()
+},
+
+'Проверка, что кнопка купить у товара под заказ называется под заказ' : function (browser) {
+  browser
+  .url(url + "/p/174523-anti-hb-markery-virusnogo-gepatita-v/")
+  .maximizeWindow()
+  .assert.containsText('.p_info td span[class="b-btn__name gradient"]', "Заказать", "Кнопка действительно называется заказать")
+  .click('.p_info td span[class="b-btn__name gradient"]', function () {
+    this.waitForElementVisible('#postavshik>div', 5000, 'Увидели кнопку отправки в корзине, довольны')
   })
-
-
-
-// проверяем, что в сообщении написано письмо отправлено
-  .waitForElementVisible('#ui-id-8', 5000)
-  .getText('#cabinet-report-block>span', function () {
-  this.assert.containsText("#cabinet-report-block>span", "Письмо отправлено.")
+  .end()
+},
+*/
+'Проверяем, что при отсутствии цены кнопка становится заказать товар' : function (browser) {
+  browser
+  .url(url + "/p/174490-anti-hcv-markery-virusnogo-gepatita-s/")
+  .maximizeWindow()
+  .assert.containsText('.p_info td span[class="b-btn__name gradient"]', "Заказать", "Кнопка действительно называется заказать товар")
+  .click('.p_info td span[class="b-btn__name gradient"]', function () {
+    this.waitForElementVisible('#send_feedback_form', 5000, 'Увидели кнопку отправки в форме написать нам, довольны')
   })
-
-
-
-}
+  .end()
+},
 
 }
